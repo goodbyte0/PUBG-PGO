21ab2ce43f5a74fa67c93303ea5e23a926590308
goodbye0 <36103562+goodbye0@users.noreply.github.com>
2018-02-22 19:16:36 +0200

    Create AssemblyInfo.cs

---
+++ PGVisual.Overlay/Properties/AssemblyInfo.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Diagnostics;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Runtime.Versioning;
+using System.Security;
+using System.Security.Permissions;
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyProduct("PGVisual.Overlay")]
+[assembly: ComVisible(false)]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: Guid("7a2cd32b-ef0f-48de-8be0-e13cc207f043")]
+[assembly: Debuggable(DebuggableAttribute.DebuggingModes.IgnoreSymbolStoreSequencePoints)]
+[assembly: RuntimeCompatibility(WrapNonExceptionThrows = true)]
+[assembly: CompilationRelaxations(8)]
+[assembly: AssemblyTitle("PGVisual.Overlay")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyDescription("")]
+[assembly: SecurityPermission(SecurityAction.RequestMinimum, SkipVerification = true)]
+[module: ConfusedBy("ConfuserEx v1.0.0")]
